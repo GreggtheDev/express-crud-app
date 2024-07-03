8fef65898efb1d38d65c44a132a8c6d566a72599
GreggtheDev <greggthedev@gmail.com>
2024-07-02 21:44:51 -0400

    started server

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ const port = 3000;
 
 // Middleware
 app.use(express.json());
+
+// Starting the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
